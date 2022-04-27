1d9dfaa95c0a949312afb769fec4246e69a61f64
Rob Simpson <rsimpson2@me.com>
2022-04-27 05:30:06 -0400

    Updates to keymaps

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -225,6 +225,11 @@ _G.packer_plugins = {
     path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/headlines.nvim",
     url = "https://github.com/lukas-reineke/headlines.nvim"
   },
+  ["impatient.nvim"] = {
+    loaded = true,
+    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/impatient.nvim",
+    url = "https://github.com/lewis6991/impatient.nvim"
+  },
   ["incsearch.vim"] = {
     config = { "require 'plugins.incsearch'" },
     loaded = true,
@@ -356,12 +361,6 @@ _G.packer_plugins = {
     loaded = true,
     only_config = true
   },
-  ["octo.nvim"] = {
-    config = { "require 'plugins.octo'" },
-    loaded = true,
-    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/octo.nvim",
-    url = "https://github.com/pwntester/octo.nvim"
-  },
   ["onedark.nvim"] = {
     config = { "require 'plugins.onedark'" },
     loaded = true,
@@ -389,11 +388,6 @@ _G.packer_plugins = {
     path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/poppy.vim",
     url = "https://github.com/bounceme/poppy.vim"
   },
-  ["popup.nvim"] = {
-    loaded = true,
-    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/popup.nvim",
-    url = "https://github.com/nvim-lua/popup.nvim"
-  },
   ["quick-scope"] = {
     loaded = true,
     path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/quick-scope",
@@ -409,11 +403,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/targets.vim",
     url = "https://github.com/wellle/targets.vim"
-  },
-  ["telescope.nvim"] = {
-    loaded = true,
-    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/telescope.nvim",
-    url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["treesitter-unit"] = {
     loaded = true,
@@ -515,6 +504,17 @@ _G.packer_plugins = {
     path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/vim-table-mode",
     url = "https://github.com/dhruvasagar/vim-table-mode"
   },
+  ["vim-test"] = {
+    loaded = true,
+    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/vim-test",
+    url = "https://github.com/vim-test/vim-test"
+  },
+  ["vim-ultest"] = {
+    config = { "require 'plugins.vim-ultest'" },
+    loaded = true,
+    path = "/Users/robsimpson/.local/share/nvim/site/pack/packer/start/vim-ultest",
+    url = "https://github.com/rcarriga/vim-ultest"
+  },
   ["virt-column.nvim"] = {
     config = { "require 'plugins.virt-column'" },
     loaded = true,
@@ -524,6 +524,62 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: onedark.nvim
+time([[Config for onedark.nvim]], true)
+require 'plugins.onedark'
+time([[Config for onedark.nvim]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+require 'plugins.comment'
+time([[Config for Comment.nvim]], false)
+-- Config for: auto-session
+time([[Config for auto-session]], true)
+require 'plugins.auto-session'
+time([[Config for auto-session]], false)
+-- Config for: lsp-format.nvim
+time([[Config for lsp-format.nvim]], true)
+require 'plugins.lsp-format'
+time([[Config for lsp-format.nvim]], false)
+-- Config for: open-browser.vim
+time([[Config for open-browser.vim]], true)
+require 'plugins.open-browser'
+time([[Config for open-browser.vim]], false)
+-- Config for: indent-blankline.nvim
+time([[Config for indent-blankline.nvim]], true)
+require 'plugins.indent-blankline'
+time([[Config for indent-blankline.nvim]], false)
+-- Config for: vim-table-mode
+time([[Config for vim-table-mode]], true)
+require 'plugins.vim-table-mode'
+time([[Config for vim-table-mode]], false)
+-- Config for: lexima.vim
+time([[Config for lexima.vim]], true)
+require 'plugins.lexima'
+time([[Config for lexima.vim]], false)
+-- Config for: vim-gitgutter
+time([[Config for vim-gitgutter]], true)
+require 'plugins.vim-gitgutter'
+time([[Config for vim-gitgutter]], false)
+-- Config for: ultisnips
+time([[Config for ultisnips]], true)
+require 'plugins.ultisnips'
+time([[Config for ultisnips]], false)
+-- Config for: bufferline.nvim
+time([[Config for bufferline.nvim]], true)
+require 'plugins.bufferline'
+time([[Config for bufferline.nvim]], false)
+-- Config for: clever-f.vim
+time([[Config for clever-f.vim]], true)
+require 'plugins.clever-f'
+time([[Config for clever-f.vim]], false)
+-- Config for: virt-column.nvim
+time([[Config for virt-column.nvim]], true)
+require 'plugins.virt-column'
+time([[Config for virt-column.nvim]], false)
+-- Config for: incsearch.vim
+time([[Config for incsearch.vim]], true)
+require 'plugins.incsearch'
+time([[Config for incsearch.vim]], false)
 -- Config for: nvim-web-devicons
 time([[Config for nvim-web-devicons]], true)
 require 'plugins.nvim-web-devicons'
@@ -532,118 +588,62 @@ time([[Config for nvim-web-devicons]], false)
 time([[Config for headlines.nvim]], true)
 require 'plugins.headlines'
 time([[Config for headlines.nvim]], false)
--- Config for: nvim-bqf
-time([[Config for nvim-bqf]], true)
-require 'plugins.nvim-bqf'
-time([[Config for nvim-bqf]], false)
--- Config for: vim-gitgutter
-time([[Config for vim-gitgutter]], true)
-require 'plugins.vim-gitgutter'
-time([[Config for vim-gitgutter]], false)
--- Config for: incsearch.vim
-time([[Config for incsearch.vim]], true)
-require 'plugins.incsearch'
-time([[Config for incsearch.vim]], false)
--- Config for: nvim-cmp
-time([[Config for nvim-cmp]], true)
-require 'plugins.nvim-cmp'
-time([[Config for nvim-cmp]], false)
--- Config for: indent-blankline.nvim
-time([[Config for indent-blankline.nvim]], true)
-require 'plugins.indent-blankline'
-time([[Config for indent-blankline.nvim]], false)
--- Config for: ack.vim
-time([[Config for ack.vim]], true)
-require 'plugins.ack'
-time([[Config for ack.vim]], false)
--- Config for: vim-table-mode
-time([[Config for vim-table-mode]], true)
-require 'plugins.vim-table-mode'
-time([[Config for vim-table-mode]], false)
--- Config for: auto-session
-time([[Config for auto-session]], true)
-require 'plugins.auto-session'
-time([[Config for auto-session]], false)
--- Config for: octo.nvim
-time([[Config for octo.nvim]], true)
-require 'plugins.octo'
-time([[Config for octo.nvim]], false)
--- Config for: virt-column.nvim
-time([[Config for virt-column.nvim]], true)
-require 'plugins.virt-column'
-time([[Config for virt-column.nvim]], false)
--- Config for: bufferline.nvim
-time([[Config for bufferline.nvim]], true)
-require 'plugins.bufferline'
-time([[Config for bufferline.nvim]], false)
--- Config for: nvim-pqf.git
-time([[Config for nvim-pqf.git]], true)
-require 'plugins.nvim-pqf'
-time([[Config for nvim-pqf.git]], false)
--- Config for: lsp-format.nvim
-time([[Config for lsp-format.nvim]], true)
-require 'plugins.lsp-format'
-time([[Config for lsp-format.nvim]], false)
--- Config for: gitsigns.nvim
-time([[Config for gitsigns.nvim]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
-time([[Config for gitsigns.nvim]], false)
--- Config for: vim-qf
-time([[Config for vim-qf]], true)
-require 'plugins.vim-qf'
-time([[Config for vim-qf]], false)
--- Config for: Comment.nvim
-time([[Config for Comment.nvim]], true)
-require 'plugins.comment'
-time([[Config for Comment.nvim]], false)
--- Config for: clever-f.vim
-time([[Config for clever-f.vim]], true)
-require 'plugins.clever-f'
-time([[Config for clever-f.vim]], false)
--- Config for: dressing.nvim
-time([[Config for dressing.nvim]], true)
-require 'plugins.dressing'
-time([[Config for dressing.nvim]], false)
--- Config for: LuaSnip
-time([[Config for LuaSnip]], true)
-require 'plugins.lua-snip'
-time([[Config for LuaSnip]], false)
--- Config for: onedark.nvim
-time([[Config for onedark.nvim]], true)
-require 'plugins.onedark'
-time([[Config for onedark.nvim]], false)
--- Config for: lexima.vim
-time([[Config for lexima.vim]], true)
-require 'plugins.lexima'
-time([[Config for lexima.vim]], false)
--- Config for: nvim-treesitter
-time([[Config for nvim-treesitter]], true)
-require 'plugins.nvim-treesitter'
-time([[Config for nvim-treesitter]], false)
--- Config for: open-browser.vim
-time([[Config for open-browser.vim]], true)
-require 'plugins.open-browser'
-time([[Config for open-browser.vim]], false)
 -- Config for: splitjoin.vim
 time([[Config for splitjoin.vim]], true)
 require 'plugins.splitjoin'
 time([[Config for splitjoin.vim]], false)
--- Config for: vim-signature
-time([[Config for vim-signature]], true)
-require 'plugins.vim-signature'
-time([[Config for vim-signature]], false)
--- Config for: ultisnips
-time([[Config for ultisnips]], true)
-require 'plugins.ultisnips'
-time([[Config for ultisnips]], false)
+-- Config for: gitsigns.nvim
+time([[Config for gitsigns.nvim]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
+time([[Config for gitsigns.nvim]], false)
+-- Config for: ack.vim
+time([[Config for ack.vim]], true)
+require 'plugins.ack'
+time([[Config for ack.vim]], false)
 -- Config for: fzf.vim
 time([[Config for fzf.vim]], true)
 require 'plugins.fzf'
 time([[Config for fzf.vim]], false)
+-- Config for: vim-signature
+time([[Config for vim-signature]], true)
+require 'plugins.vim-signature'
+time([[Config for vim-signature]], false)
+-- Config for: LuaSnip
+time([[Config for LuaSnip]], true)
+require 'plugins.lua-snip'
+time([[Config for LuaSnip]], false)
 -- Config for: Navigator.nvim
 time([[Config for Navigator.nvim]], true)
 require 'plugins.navigator'
 time([[Config for Navigator.nvim]], false)
+-- Config for: nvim-cmp
+time([[Config for nvim-cmp]], true)
+require 'plugins.nvim-cmp'
+time([[Config for nvim-cmp]], false)
+-- Config for: dressing.nvim
+time([[Config for dressing.nvim]], true)
+require 'plugins.dressing'
+time([[Config for dressing.nvim]], false)
+-- Config for: nvim-bqf
+time([[Config for nvim-bqf]], true)
+require 'plugins.nvim-bqf'
+time([[Config for nvim-bqf]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+require 'plugins.nvim-treesitter'
+time([[Config for nvim-treesitter]], false)
+-- Config for: nvim-pqf.git
+time([[Config for nvim-pqf.git]], true)
+require 'plugins.nvim-pqf'
+time([[Config for nvim-pqf.git]], false)
+-- Config for: vim-qf
+time([[Config for vim-qf]], true)
+require 'plugins.vim-qf'
+time([[Config for vim-qf]], false)
+-- Config for: vim-ultest
+time([[Config for vim-ultest]], true)
+require 'plugins.vim-ultest'
+time([[Config for vim-ultest]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd neo-tree.nvim ]]
