9d8deab0beeb1d41db335c78b37b2f73e3154cdf
Rob Simpson <rsimpson@ventera.com>
2022-05-11 14:17:55 -0400

    add ideas from astrovim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,14 @@
 require('telescope').setup {
     defaults = {
         layout_config = {
-            vertical = { width = 0.8 },
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = { mirror = false },
+            width = 0.90,
+            height = 0.90
         },
         mappings = {
             i = {
