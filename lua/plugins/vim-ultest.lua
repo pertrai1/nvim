5eaee85874b9edf17ab1c9afc5bd8e2b91cd7266
Rob Simpson <rsimpson2@me.com>
2022-05-15 14:42:17 -0400

    fix jest

--- lua/plugins/vim-ultest.lua
+++ lua/plugins/vim-ultest.lua
@@ -1,2 +1,2 @@
 vim.g['test#javascript#runner'] = 'jest'
-vim.g['test#javascript#jest#options'] = '--reporters jest-vim-reporter'
+-- vim.g['test#javascript#jest#options'] = '--reporters jest-vim-reporter'
